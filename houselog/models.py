88cf21d917cc091fc611c61a61eaead7dc8d07d4
aliang <aliang@duosecurity.com>
2023-03-07 21:23:27 -0500

    initial commit

---
+++ houselog/models.py
@@ -0,0 +1,14 @@
+import datetime
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Houselog(models.Model):
+    title = models.CharField(max_length=200)
+    last_done = models.DateField(auto_now_add=True)
+    frequency = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    @property
+    def next_run(self):
+        return self.last_done + datetime.timedelta(days=self.frequency)
